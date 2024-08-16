04b9c48358cc24be7f62e04e878eb3a169632ca8
fayshal69 <foyshalbinamir@gmail.com>
2024-08-16 22:32:46 +0600

    update: navbar

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -36,7 +36,7 @@ const NavBar = () => {
                             </a>
                         </div>
                         <button onClick={() => setIsToggleOpen(!isToggleOpen)} 
-                            className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 ms-3"
+                            className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden focus:outline-none  ms-3"
                             aria-controls="mobile-menu-2" aria-expanded="true">
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
