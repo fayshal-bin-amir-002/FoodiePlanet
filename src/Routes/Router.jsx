b8a02cf404df76218ef2b4010199bbd3e4d34f62
fayshal69 <foyshalbinamir@gmail.com>
2024-08-16 23:16:37 +0600

    add: 404 error page

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,18 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
+import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <MainLayout></MainLayout>,
-      children: [
-        {
-            path: "/",
-            element: <Home></Home>
-        }
-      ]
-    },
-  ]);
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
 
 export default router;
